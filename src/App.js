3c43e11fe8db336dde5fa76eb48f8b17fade54d8
hryesf <houriye.esfahanian@gmail.com>
2024-03-08 13:34:45 +0330

    remove comments

--- src/App.js
+++ src/App.js
@@ -130,7 +130,6 @@ function App() {
                     </Grid>
                 </Grid>
                 <Grid item xs={12} md={6}>
-                    {/*<WeeklyForecast data={weekForecast}/>*/}
                     {weeklyForecastComponent}
                 </Grid>
             </React.Fragment>
